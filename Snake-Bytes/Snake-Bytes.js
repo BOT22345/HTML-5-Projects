480305086ca025a104f660b3dd42d668969b5a0f
BOT22345 <nmongia48@gmail.com>
2024-12-19 21:50:12 +0530

    added the three sections in JS and created sound variables for Snake-Bytes

---
+++ Snake-Bytes/Snake-Bytes.js
@@ -0,0 +1,12 @@
+//varialbes
+let direction ={x:0,y:0};
+const foodSound =new Audio("./Assets/sounds/food.mp3");
+const gameOverSound=new Audio("./Assets/sounds/gameOver.mp3");
+const moveSound=new Audio("./Assets/sounds/move.mp3");
+const musicSound=new Audio("./Assets/sounds/music.mp3");
+
+
+//functions
+
+
+//main logic
