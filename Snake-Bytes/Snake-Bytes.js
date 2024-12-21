cf99a56bc57748c74b19b538ed06929337df0653
BOT22345 <nmongia48@gmail.com>
2024-12-21 18:03:56 +0530

    added the frame rate controlling for game in Snake-Bytes

--- Snake-Bytes/Snake-Bytes.js
+++ Snake-Bytes/Snake-Bytes.js
@@ -4,11 +4,22 @@ const foodSound =new Audio("./Assets/sounds/food.mp3");
 const gameOverSound=new Audio("./Assets/sounds/gameOver.mp3");
 const moveSound=new Audio("./Assets/sounds/move.mp3");
 const musicSound=new Audio("./Assets/sounds/music.mp3");
-
+let speed=2;
+let lastPaintTime=0;
 
 //functions
 function main(ctime){
     window.requestAnimationFrame(main);
+    if((ctime-lastPaintTime)/1000<1/speed){
+        return ;
+    }else{
+        lastPaintTime=ctime;
+        gameEngine();
+    }
+}
+
+function gameEngine(){
+    
 }
 
 //main logic
