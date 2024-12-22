6677f84bc2ecf07c8c80156f35e6544f4281b550
BOT22345 <nmongia48@gmail.com>
2024-12-22 22:12:31 +0530

    created the snakeElement and appended it to board using JS

--- Snake-Bytes/Snake-Bytes.js
+++ Snake-Bytes/Snake-Bytes.js
@@ -4,8 +4,12 @@ const foodSound =new Audio("./Assets/sounds/food.mp3");
 const gameOverSound=new Audio("./Assets/sounds/gameOver.mp3");
 const moveSound=new Audio("./Assets/sounds/move.mp3");
 const musicSound=new Audio("./Assets/sounds/music.mp3");
+let board = document.querySelector(".board");
 let speed=2;
 let lastPaintTime=0;
+let snakeArr=[
+    {x:13,y:15},
+];
 
 //functions
 function main(ctime){
@@ -19,7 +23,17 @@ function main(ctime){
 }
 
 function gameEngine(){
-    
+    // Part 1: Updating the snake array and food 
+
+    //Part 2" Display the snake array and food
+    board.innerHTML="";
+    snakeArr.forEach((e,index)=>{
+        snakeElement=document.createElement('div');
+        snakeElement.style.gridRowStart=e.y;
+        snakeElement.style.gridColumnStart=e.x;
+        snakeElement.classList.add('food');
+        board.appendChild(snakeElement);
+    })
 }
 
 //main logic
