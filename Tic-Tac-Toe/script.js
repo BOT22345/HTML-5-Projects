bdcb236a740e915d6f60626ee5f914d839a9e1b4
BOT22345 <nmongia48@gmail.com>
2025-02-04 11:14:34 +0530

    added the logic for setting current player symbol to gameboard array and actual cell in dom

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -10,6 +10,10 @@ const winPatters=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6
 // Handle cell clicks 
 cells.forEach((cell,index)=>{
     cell.addEventListener("click",()=>{
+        if (gameBoard[index] === "" && !checkWinner()){
+            gameBoard[index]=currentPlayer;
+            cell.innerHTML=currentPlayer;
+        }
         
     })
 })
