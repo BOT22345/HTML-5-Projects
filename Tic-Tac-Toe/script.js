2de230fbe2bc519b483077f4026526aa31f3c1a2
BOT22345 <nmongia48@gmail.com>
2025-02-05 12:55:31 +0530

    added the logic for winning conditions and checkWinner function

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -5,7 +5,7 @@ const resetButton=document.getElementById("reset");
 let currentPlayer="X";
 let gameBoard=["","","","","","","","",""];
 
-const winPatters=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+const winPatterns=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
 // Handle cell clicks 
 cells.forEach((cell,index)=>{
@@ -13,7 +13,28 @@ cells.forEach((cell,index)=>{
         if (gameBoard[index] === "" && !checkWinner()){
             gameBoard[index]=currentPlayer;
             cell.innerHTML=currentPlayer;
+        
+            if(checkWinner()){
+                statusText.innerHTML=`Player ${currentPlayer} Wins`;
+            }
+            else if (gameBoard.every(cell => cell!== "")){
+                statusText.innerHTML="It's a draw";
+            }
+            else{
+                currentPlayer=currentPlayer==="X" ?"O":"X";
+                statusText.innerHTML=`Player ${currentPlayer} Turn`;
+            }
+
         }
         
     })
 })
+
+//check winner function 
+function checkWinner(){
+    return winPatterns.some(pattern=>{
+        const [a,b,c]=pattern;
+        return(gameBoard[a] && gameBoard[a]===gameBoard[b] && gameBoard[a]===gameBoard[c]);
+    }
+    )
+}
